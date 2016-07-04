18f7898fa531e3418d2c504086e604715f788db6
fnagel <elfixlange@gmail.com>
2016-07-04 08:14:39 +0200

    --

--- src/Path.php
+++ src/Path.php
@@ -4,22 +4,14 @@ namespace LuckyNail\Helper;
 
 class Path{
 	public static function to_path_part($sString){
-		$sString = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $sString);
-		return DIRECTORY_SEPARATOR.trim($sString, DIRECTORY_SEPARATOR);
+		return trim(str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $sString), DIRECTORY_SEPARATOR);
 	}
 	public static function to_abs_path($sString){
-		$sString = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $sString);
-		return realpath($sString);
+		return realpath(str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $sString));
 	}
 	public static function to_url_part($sString){
-		$sString = str_replace([DIRECTORY_SEPARATOR, '\\'], '/', $sString);
-		return trim($sString, '/');
+		return trim(str_replace([DIRECTORY_SEPARATOR, '\\'], '/', $sString), '/');
 	}
-	/**
-	 * Gibt die Extension eines Dateinamens zurück (ohne Punkt).
-	 * @param 	string 	$sFileName 	Der Dateiname (NICHT Pfad)
-	 * @return 	string 				Die Dateiextension (ohne Punkt)
-	 */
 	public static function get_file_extension($sFileName){
 		preg_match("=[^\.]+$=i", $sFileName, $aMatches);
 		return array_pop($aMatches);
